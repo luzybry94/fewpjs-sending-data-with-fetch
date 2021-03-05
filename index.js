85d737c935de9a673025ff01e136dd35a5d635be
luzybry94 <luzybry94@gmail.com>
2021-03-05 12:55:05 -0500

    commit tree 8a99d47e0324845fd81681bcc7909a5020c22a32

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name: userName,
+        email: userEmail
+    })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        const id = object.id
+        document.body.textContent = id;
+    })
+    .catch(function(error) {
+        document.body.textContent = error.message
+      });
+}
